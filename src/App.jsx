7d53ed93b382c0da4ef281ff5a9caf63762a628f
Nuggdr <nuggetmex003@outlook.com>
2024-10-28 02:19:50 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Footer from "./components/Footer";
+import Pricing from "./components/Pricing";
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
